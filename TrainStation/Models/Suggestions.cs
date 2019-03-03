0657d8041012377ebe79d76b8f90322cb229b597
Ferenc Schultesz <ferenc.schultesz@gmail.com>
2019-03-03 20:16:14 +0000

    Comments added to Suggestions.cs and ListSuggestorService.cs

--- TrainStation/Models/Suggestions.cs
+++ TrainStation/Models/Suggestions.cs
@@ -4,11 +4,17 @@ using System.Text;
 
 namespace TrainStation.Models
 {
+    /// <summary>
+    /// Suggestions class include the next letters and possible stations depending on the current user input
+    /// </summary>
     public class Suggestions
     {
         public List<char> NextLetters { get; set; }
         public List<string> Stations { get; set; }
 
+        /// <summary>
+        /// Constructor to init Suggestions
+        /// </summary>
         public Suggestions()
         {
             NextLetters = new List<char>();
--- TrainStation/Services/ListSuggestorService.cs
+++ TrainStation/Services/ListSuggestorService.cs
@@ -7,22 +7,37 @@ using TrainStation.Utils;
 
 namespace TrainStation.Services
 {
+    /// <summary>
+    /// ITrainStationSuggestorService implekmentation by using List as datastructure to hold stations.
+    /// </summary>
     public class ListSuggestorService : ITrainStationSuggestorService
     {
         private List<string> stations;
         private IFileHandler fileHandler;
+
+        /// <summary>
+        /// Constructor takes the data file path and a FileHandler and populates the stations list
+        /// </summary>
+        /// <param name="dataFilePath">Path of the data file</param>
+        /// <param name="_fileHandler">Object that implements the IFileHandler interface to deal with the input data file.</param>
         public ListSuggestorService(string dataFilePath, IFileHandler _fileHandler)
         {
             this.fileHandler = _fileHandler;
             this.stations = fileHandler.ReadTextFileLines(dataFilePath);
         }
 
+        /// <summary>
+        /// Gets the suggestions based on the user input
+        /// </summary>
+        /// <param name="userInput">User input</param>
+        /// <returns>Suggestions including possible next letters and stations.</returns>
         public Suggestions GetSuggestions(string userInput)
         {
             userInput = userInput.ToUpper();
             var suggestions = new Suggestions();
 
-            // Get all stations
+            // Get all stations from stations list that start with the user input
+            // If the user input is empty, get all stations
             if (string.IsNullOrEmpty(userInput))
             {
                 suggestions.Stations = this.stations;
@@ -32,18 +47,23 @@ namespace TrainStation.Services
                 suggestions.Stations = this.stations.Where(s => s.ToLower().StartsWith(userInput.ToLower())).ToList();
             }
             
-            // Get possible next chars
-            List<char> chars = new List<char>();
+            // Get all possible next letters
+            // Loop through on all suggested stations
             foreach (string station in suggestions.Stations)
             {
                 string sub = station.Substring(userInput.Length);
-                if (sub.Length >0)
+                // If there are more letters after taking the prefix out of the station
+                if (sub.Length > 0)
                 {
-                    chars.Add(sub[0]);
+                    // Add the next letter (char) to the suggested next letters
+                    suggestions.NextLetters.Add(sub[0]);
                 }
             }
             
-            suggestions.NextLetters = chars.Distinct().ToList();
+            // Only need distinct characters
+            suggestions.NextLetters = suggestions.NextLetters.Distinct().ToList();
+            // Order the list
+            suggestions.NextLetters.Sort();
             return suggestions;
         }
     }
