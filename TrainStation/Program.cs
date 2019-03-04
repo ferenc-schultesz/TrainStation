9a9957ad2b35b30dcdc0794d7f517fa794a691e5
Ferenc Schultesz <ferenc.schultesz@smartr365.com>
2019-03-04 13:12:15 +0000

    Added a performance test function

--- TrainStation/Program.cs
+++ TrainStation/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using TrainStation.Models;
 using TrainStation.Services;
 using TrainStation.Utils;
 
@@ -9,91 +12,129 @@ namespace TrainStation
     {
         static void Main(string[] args)
         {
-            ITrainStationSuggestorService suggestorWithList;
             ITrainStationSuggestorService suggestorWithTrie;
-            IFileHandler ffileHAndler = new FileHandler();
+            IFileHandler fileHAndler = new FileHandler("Data\\TrainStations.txt");
+
             try
             {
                 // Init suggestors
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-                suggestorWithList = new TrieSuggestorService("Data\\TrainStations.txt", ffileHAndler);
-                stopwatch.Stop();
-                Console.WriteLine("Trainstation suggester has been initialised.");
-                Console.Write($"It took {stopwatch.ElapsedTicks} ticks with list data structure ");
-                stopwatch.Reset();
-                stopwatch.Start();
-                suggestorWithTrie = new TrieSuggestorService("Data\\TrainStations.txt", ffileHAndler);
-                stopwatch.Stop();
-                Console.WriteLine($"and {stopwatch.ElapsedTicks} ticks with trie data structure.");
-                stopwatch.Reset();
+                suggestorWithTrie = new TrieSuggestorService(fileHAndler);
+                Console.WriteLine("------ Trainstation suggester has been initialised ------");
 
-                // Get user input and search
-                string searchString;
-                Console.WriteLine("Enter full or partial station name: ");
-                searchString = Console.ReadLine();
+                // Tests the suggestor implementation with user input
+                string userInput = TestSuggestor(ref suggestorWithTrie);
 
-                // search with list 
-                stopwatch.Start();
-                var listSuggestions = suggestorWithList.GetSuggestions(searchString);
-                stopwatch.Stop();
-                Console.WriteLine($"------------- Search took {stopwatch.ElapsedTicks} ticks with list -------------");
-                Console.Write("Possible next letters: ");
-                foreach(char letter in listSuggestions.NextLetters)
-                {
-                    Console.Write(letter + " ");
-                }
-                Console.WriteLine("");
-                Console.WriteLine("------------------------------------");
-                Console.Write("Possible Stations: ");
-                foreach (string station in listSuggestions.Stations)
-                {
-                    Console.Write(station + " ");
-                }
-                Console.WriteLine("");
-                stopwatch.Reset();
-
-                // search with trie
-                stopwatch.Start();
-                var trieSuggestions = suggestorWithTrie.GetSuggestions(searchString);
-                stopwatch.Stop();
-                Console.WriteLine($"Search took {stopwatch.ElapsedTicks} ticks with trie ------------------------------------");
-                Console.Write("Possible next letters: ");
-                foreach (char letter in trieSuggestions.NextLetters)
-                {
-                    Console.Write(letter + " ");
-                }
-                Console.WriteLine("");
-                Console.WriteLine("------------------------------------");
-                Console.Write("Possible Stations: ");
-                foreach (string station in trieSuggestions.Stations)
-                {
-                    Console.Write(station + " ");
-                }
-                stopwatch.Reset();
-
-                stopwatch.Start();
-                for (int i = 0; i < 100000; i++)
-                {
-
-                }
-                Console.WriteLine($"Loop test 1: {stopwatch.ElapsedTicks}");
-                stopwatch.Reset();
-
-                stopwatch.Start();
-                for (int i = 0; i < 100000; i++)
-                {
-
-                }
-                Console.WriteLine($"Loop test 2: {stopwatch.ElapsedTicks}");
-                stopwatch.Reset();
+                // Meassures and displays performance difference between Trie and List implementation
+                // ListSuggestorService uses List and Linq, Trie uses Trie search tree data structure
+                CheckPerformance();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
             Console.Read();
+        }
+
+        public static string TestSuggestor(ref ITrainStationSuggestorService service)
+        {
+            // Get user input and search
+            string userInput;
+            Console.WriteLine("Enter full or partial station name: ");
+            userInput = Console.ReadLine();
+
+            // search with trie
+            var trieSuggestions = service.GetSuggestions(userInput);
+            Console.Write("Possible next letters: ");
+            foreach (char letter in trieSuggestions.NextLetters)
+            {
+                Console.Write(letter + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------------------------");
+            Console.Write("Possible Stations: ");
+            foreach (string station in trieSuggestions.Stations)
+            {
+                Console.Write(station + " ");
+            }
+            Console.WriteLine("");
+            return userInput;
+        }
+
+        /// <summary>
+        /// Page faults, static initializers, JIT, CPU cache, branch predictors, and context switches affect run time
+        /// therefore performance is checked in initiaising multiple times, and seach performance checked by searching
+        /// for multiple (randomly generated) user inputs
+        /// </summary>
+        public static void CheckPerformance()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            IFileHandler fileHandler = new FileHandler("Data\\TrainStations.txt");
+            ITrainStationSuggestorService trieService;
+            ITrainStationSuggestorService listService;
+            trieService = new TrieSuggestorService(fileHandler);
+            listService = new ListSuggestorService(fileHandler);
+
+            // Performance check for initialisation (building the stations lists
+            int numOfInit = 100;
+            stopwatch.Start();
+            for (int i = 0; i < numOfInit; ++i)
+            {
+                trieService = new TrieSuggestorService(fileHandler);
+            }
+            stopwatch.Start();
+            var trieInitPerf = stopwatch.ElapsedTicks / numOfInit;
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            for (int i = 0; i < 100; ++i)
+            {
+                listService = new ListSuggestorService(fileHandler);
+            }
+            stopwatch.Start();
+            var listInitPerf = stopwatch.ElapsedTicks / numOfInit;
+            stopwatch.Reset();
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"Trie initialisation average performance: { trieInitPerf } ticks.");
+            Console.WriteLine($"List initialisation average performance: { listInitPerf } ticks.");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Generating test user inputs for search performance test...");
+
+            // Performance check for search
+            Suggestions result;
+            Random random = new Random();
+            int numOfTestInput = 50;
+            var userInputs = fileHandler.GetRandomStationPrefixes(numOfTestInput);
+            Console.Write("Generated inputs: ");
+            foreach (string input in userInputs)
+            {
+                Console.Write($"{input} ");
+            }
+            Console.WriteLine("");
+
+            stopwatch.Start();
+            foreach (string input in userInputs)
+            {
+                result = trieService.GetSuggestions(input);
+            }
+            stopwatch.Stop();
+            var trieSearchPerf = stopwatch.ElapsedTicks / numOfTestInput;
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            foreach (string input in userInputs)
+            {
+                result = listService.GetSuggestions(input);
+            }
+            stopwatch.Stop();
+            var listSearchPerf = stopwatch.ElapsedTicks / numOfTestInput;
+            stopwatch.Reset();
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"Trie search average performance: { trieSearchPerf } ticks.");
+            Console.WriteLine($"List search average performance: { listSearchPerf } ticks.");
         }
     }
 }
